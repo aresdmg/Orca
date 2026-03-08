a0334eb6bc7c92297e3908f6b872376ae8f7c500
Ares <thisisares01@gmail.com>
2026-03-09 00:31:58 +0530

    feat: config added for fastify server

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import Fastify from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import config from "./config/config";
 
 export default function bootstarp() {
     const app = Fastify({
         logger: true
-    })
+    }).withTypeProvider<ZodTypeProvider>()
+    
+    config(app)
     
     return app
 } 
---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+import cors from "@fastify/cors";
+import helmet from "@fastify/helmet";
+import jwt from "@fastify/jwt";
+import sensible from "@fastify/sensible";
+import cookies from "@fastify/cookie";
+import { FastifyInstance } from "fastify";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+
+export default function config(app: FastifyInstance) {
+    app.register(cors)
+    app.register(helmet)
+    app.register(jwt,
+        {
+            secret: process.env.JWT_SECRET!,
+            sign: {
+                expiresIn: "15m"
+            }
+        }
+    )
+    app.register(sensible)
+    app.register(cookies)
+    
+    app.setValidatorCompiler(validatorCompiler)
+    app.setSerializerCompiler(serializerCompiler)
+}
